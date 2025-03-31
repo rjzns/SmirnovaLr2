49cf448277373b7f38386cf2c7e10602a86ad2e2
rjzns <mhk.jrsh139@gmail.com>
2025-03-31 10:45:00 +0300

    Добавлены обработки в файл myform.py, обработка заполнения, верного формата почты, добавлено поле username в файл index.tpl

--- myform.py
+++ myform.py
@@ -1,7 +1,25 @@
 from bottle import post, request
+import re
+from datetime import datetime
+
+# Регулярное выражение для проверки email
+EMAIL_PATTERN = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
 @post('/home', method='post')
 def my_form():
     mail = request.forms.get('ADRESS')
-    return "Thanks! The answer will be sent to the mail %s" % mail
+    username = request.forms.get('USERNAME')
+    
+    # Проверка заполненности полей
+    if not mail or not username:
+        return "Error: All fields must be filled!"
+    
+    # Проверка формата email
+    if not re.match(EMAIL_PATTERN, mail):
+        return "Error: Invalid email format!"
+    
+    # Получение текущей даты в сокращённом формате
+    access_date = datetime.now().strftime('%Y-%m-%d')
+    
+    return "Thanks, %s! The answer will be sent to the mail %s. Access Date: %s" % (username, mail, access_date)
 
