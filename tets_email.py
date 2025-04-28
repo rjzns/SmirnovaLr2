08de15994b8271bdee8f4ce90b0edc9124296a25
rjzns <mhk.jrsh139@gmail.com>
2025-04-28 10:17:18 +0300

    Добавление новых тестов, изменение регулярного выражения

--- tets_email.py
+++ tets_email.py
@@ -1,45 +1,54 @@
 import unittest
 import re
-from myform import EMAIL_PATTERN  
+from myform import EMAIL_PATTERN
 
-class TestEmailValidation(unittest.TestCase):
+class TestEmailPattern(unittest.TestCase):
     
     def test_invalid_emails(self):
-  
-        list_mail_uncor = [
-            "",                   
-            "1",                   
-            "m1@",                 
-            "@mail",               
-            "m1@mail",            
-            "m1@.ru",              
-            "m1.mail.ru",          
-            "m@ma@il.ru",          
-            "m1#mail.ru",          
-            " m1@mail.ru",         
-            "m1@mail.ru ",         
-            "m1@ma il.ru"          
+        invalid_emails = [
+            "",
+            "plain",
+            "@domain.com",
+            "user@.com",
+            "user@domain",
+            "user@domain..com",
+            "user..name@domain.com",
+            "user@domain@domain.com",
+            "user name@domain.com",
+            "user@domain com",
+            "user#@domain.com",
+            ".user@domain.com",
+            "user.@domain.com",
+            "user@domain-.com",
+            "user@domain,com"
         ]
         
-  
-        for email in list_mail_uncor:
+        for email in invalid_emails:
             with self.subTest(email=email):
-                self.assertFalse(re.match(EMAIL_PATTERN, email), f"Email {email} uncorrect")
+                self.assertFalse(bool(re.match(EMAIL_PATTERN, email)), f"Expected {email} to be invalid")
 
     def test_valid_emails(self):
-
-        list_mail_cor = [
-            "m.m@mail.ru",
-            "m1@gmail.com",
-            "user123@domain.co.uk",
-            "test.email@subdomain.example.com",
-            "name+tag@domain.org"
+        valid_emails = [
+            "simple@example.com",
+            "user.name@domain.co",
+            "user+tag@subdomain.org",
+            "first-last@domain.net",
+            "123user@domain.info",
+            "user@sub.sub.domain.com",
+            "a@b.co",
+            "user_name@domain.edu",
+            "test.email@domain.travel",
+            "user123@domain.club",
+            "name@domain.io",
+            "user@123domain.com",
+            "x.y.z@domain.biz",
+            "user-abc@domain.shop",
+            "email@domain.museum"
         ]
         
-
-        for email in list_mail_cor:
+        for email in valid_emails:
             with self.subTest(email=email):
-                self.assertTrue(re.match(EMAIL_PATTERN, email), f"Email {email} correct")
+                self.assertTrue(bool(re.match(EMAIL_PATTERN, email)), f"Expected {email} to be valid")
 
 if __name__ == '__main__':
     unittest.main()
